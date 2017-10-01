ae77d1c84ef8f19b830541d28cf9592b74fbefdf
hecey <hecey@hotmail.com>
2017-09-30 23:51:31 -0500

    Parse Json and handle Exception

--- app/src/main/java/com/katrina/climapm/WeatherController.java
+++ app/src/main/java/com/katrina/climapm/WeatherController.java
@@ -157,7 +157,7 @@ public class WeatherController extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 Log.d("Clima", "Success! JSON: " + response.toString());
-
+                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
             }
 
             @Override
